450b1081020569f6bdef5f654c682659e2563ad1
yangdonglin <332407470@qq.com>
2021-02-04 14:39:54 +0800

    rustfmt

--- filecoin-proofs/src/api/custom/c2/mod.rs
+++ filecoin-proofs/src/api/custom/c2/mod.rs
@@ -55,7 +55,8 @@ mod stage2;
 
 lazy_static! {
     pub static ref SECTOR_ID: SectorId = SectorId::from(
-		std::env::args().nth(4)
+        std::env::args()
+            .nth(4)
             .and_then(|s| s.parse().ok())
             .unwrap_or(0)
     );
